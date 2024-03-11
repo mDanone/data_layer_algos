2535ffb4ff79cf7441630784a0f3e776c7f03736
mDanone <daniyalbekow@icloud.com>
2024-03-12 02:38:43 +0300

    refactored convolutional code made pre transition calculation

--- src/frame_fix_algos/nasa_convo_code.rs
+++ src/frame_fix_algos/nasa_convo_code.rs
@@ -1,19 +1,21 @@
-use std::collections::{VecDeque, HashMap};
+use std::collections::{HashMap};
+
+use crate::utils::convert_char_bit_to_int;
 
 
 struct BitSM {
-    registers: VecDeque<char>,
+    registers: String,
 }
 
 impl BitSM {
 
     fn build_empty_states_map(&self) -> HashMap<String, HashMap<String, String>> {
-        let registers_len = self.registers.len();
+        let k = self.get_register_size() + 1;
         let mut states_map = HashMap::new();
-        for val in 0..usize::pow(2, (registers_len - 1) as u32)  {
+        for val in 0..usize::pow(2, (k - 1) as u32)  {
             let mut binary_number = format!("{:b}", val);
             binary_number =
-                "0".repeat(registers_len - binary_number.len() - 1)
+                "0".repeat(k - binary_number.len() - 1)
                     + &binary_number.clone();
             states_map.insert(binary_number, HashMap::new());
         }
@@ -24,58 +26,74 @@ impl BitSM {
         let mut empty_states_map = self.build_empty_states_map();
         for (key, val) in &mut empty_states_map {
             let zero_transition = format!("{}{}", "0", key);
-            let (first_control_bit, second_control_bit) = self.get_next_control_bits(&zero_transition);
+            let (first_control_bit, second_control_bit) = self.get_next_control_bits('0', key);
             val.insert(zero_transition[..key.len()].to_string(), format!("{first_control_bit}{second_control_bit}"));
 
             let one_transition = format!("{}{}", "1", key);
-            let (first_control_bit, second_control_bit) = self.get_next_control_bits(&one_transition);
+            let (first_control_bit, second_control_bit) = self.get_next_control_bits('1', key);
             val.insert(one_transition[..key.len()].to_string(), format!("{first_control_bit}{second_control_bit}"));
         }
         empty_states_map
     }
 
 
-    fn get_first_control_bit(&self, state: &String) -> u32 {
+    fn get_first_control_bit(&self, next_bit: char, state: &String) -> u32 {
         let index_sequence = [1, 2, 4, 5];
-        self.sum_control_bits(index_sequence, &state)
+        self.sum_control_bits(index_sequence, next_bit, &state)
     }
 
-    fn sum_control_bits(&self, indexes: [usize; 4], state: &String) -> u32{
+    fn sum_control_bits(&self, indexes: [usize; 4], next_bit: char, state: &String) -> u32{
         let mut transition= 0;
+        transition ^= convert_char_bit_to_int(next_bit);
         for index in indexes {
-            transition ^= state.as_bytes()[index] as u32;
+            let x = convert_char_bit_to_int(state.as_bytes()[index] as char);
+            transition ^= x;
         }
         transition
     }
 
-    fn get_second_control_bit(&self, state: &String) -> u32 {
-        let index_sequence = [0, 1, 3, 5];
-        self.sum_control_bits(index_sequence, state)
+    fn get_second_control_bit(&self, next_bit: char, state: &String) -> u32 {
+        let index_sequence = [0, 1, 2, 5];
+        self.sum_control_bits(index_sequence, next_bit, state)
     }
 
-    fn get_next_control_bits(&self, state: &String) -> (u32, u32){
+    fn get_next_control_bits(&self, next_bit: char, state: &String) -> (u32, u32){
         let (first_bit, second_bit) = (
-            self.get_first_control_bit(&state),
-            self.get_second_control_bit(&state)
+            self.get_first_control_bit(next_bit, &state),
+            self.get_second_control_bit(next_bit, &state)
         );
 
         (first_bit, second_bit)
     }
 
     fn get_register_size(&self) -> usize {self.registers.len()}
+
+    fn shift_register(&mut self, next_bit: char) {
+        self.registers = format!("{}{}", next_bit, &self.registers[..self.registers.len() - 1]);
+    }
+
+    fn get_current_state(&self) -> &str {
+        &self.registers
+    }
 }
 
 
-// fn encode(mut frame: String, bit_sm: &mut BitSM) -> String {
-//     let mut result_frame = String::new();
-//
-//     frame.push_str("0".repeat(bit_sm.get_register_size()).as_str());
-//     while !frame.is_empty() {
-//         // let (first_control_bit, second_control_bit) = bit_sm.get_next_control_bits(frame.remove(0));
-//         result_frame.push_str(format!("{first_control_bit}{second_control_bit}").as_str());
-//     }
-//     result_frame
-// }
+fn encode(mut frame: String, bit_sm: &mut BitSM) -> String {
+    let mut result_frame = String::new();
+    let states_map = bit_sm.states_map();
+    println!("{:?}", states_map);
+
+    while !frame.is_empty() {
+        let next_frame_bit = frame.remove(0);
+        let current_state = bit_sm.get_current_state();
+        let current_state_map = states_map.get(current_state).unwrap();
+        bit_sm.shift_register(next_frame_bit);
+        let next_state = bit_sm.get_current_state();
+        let output_bits = current_state_map.get(next_state).unwrap();
+        result_frame = format!("{}{}", output_bits, result_frame);
+    }
+    result_frame
+}
 
 fn decode(frame: String) -> String {
     // let output_frame_len = frame.len() / 2;
@@ -87,21 +105,17 @@ fn decode(frame: String) -> String {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn frame_encoded_1() {
-    //     let frame = String::from("111");
-    //     let registers = VecDeque::from(['0', '0', '0', '0', '0', '0']);
-    //     let mut bit_sm = BitSM {registers};
-    //     let encoded_frame = encode(frame, &mut bit_sm);
-    //     assert_eq!(
-    //         encoded_frame,
-    //         "111001000001000111"
-    //     );
-    //     assert_eq!(
-    //         bit_sm.registers,
-    //         VecDeque::from(['0', '0', '0', '0', '0', '0'])
-    //     );
-    // }
+    #[test]
+    fn frame_encoded_1() {
+        let frame = String::from("111");
+        let mut bit_sm = BitSM {registers: String::from("000000")};
+        let encoded_frame = encode(frame, &mut bit_sm);
+        println!("{}", encoded_frame);
+        assert_eq!(
+            encoded_frame,
+            "011011"
+        );
+    }
 
     #[test]
     fn frame_decoded_1() {
@@ -115,7 +129,7 @@ mod tests {
 
     #[test]
     fn empty_states_map_build() {
-        let bit_sm = BitSM {registers: VecDeque::from(['0', '0', '0', '0', '0', '0'])};
+        let bit_sm = BitSM {registers: String::from("000000")};
 
         let states_map = bit_sm.states_map();
         println!("{:?}", states_map);
